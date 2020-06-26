023fa7bedb1ff288e36165b587880b005688384c
Eduardo Gasparotto <gasp.eduardo@gmail.com>
2020-06-25 21:46:03 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoIA.Apresentacao/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using ProjetoIA.Dominio;
+using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace ProjetoIA.Apresentacao
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Ponto ponto;
+
+        private static readonly Action EmptyDelegate = delegate { };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ponto = new Ponto("Ponto1", grdLabirinto);
+        }
+
+        private void btnProximo_Click(object sender, RoutedEventArgs e)
+        {
+            ponto.Norte();
+            ponto.Norte();
+            ponto.Leste();
+            ponto.Oeste();
+        }
+    }
+}
---
+++ ProjetoIA.Dominio/Ponto.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace ProjetoIA.Dominio
+{
+    public class Ponto : Label
+    {
+        public Ponto(string nome, Grid grid)
+        {
+            Name = nome;
+            Content = "P";
+            Width = 30;
+            Height = 30;
+            Margin = new Thickness(0, 21, 0, 0);
+            Foreground = new SolidColorBrush(Colors.White);
+            Background = new SolidColorBrush(Colors.Black);
+            HorizontalAlignment = HorizontalAlignment.Center;
+            VerticalAlignment = VerticalAlignment.Center;
+
+            Grid.SetRow(this, 3);
+            Grid.SetColumn(this, 0);
+            grid.Children.Add(this);
+        }
+
+        private void Refresh(Action funcao)
+        {
+            Dispatcher.Invoke(DispatcherPriority.Background, funcao);
+        }
+
+        public void Norte()
+        {
+            Refresh(() =>
+            {
+                Grid.SetRow(this, Grid.GetRow(this) - 1);
+            });
+            Thread.Sleep(1000);
+        }
+        public void Sul()
+        {
+            Refresh(() =>
+            {
+                Grid.SetRow(this, Grid.GetRow(this) + 1);
+            });
+            Thread.Sleep(1000);
+        }
+        public void Leste()
+        {
+            Refresh(() =>
+            {
+                Grid.SetColumn(this, Grid.GetColumn(this) + 1);
+            });
+            Thread.Sleep(1000);
+        }
+        public void Oeste()
+        {
+            Refresh(() =>
+            {
+                Grid.SetColumn(this, Grid.GetColumn(this) - 1);
+            });
+            Thread.Sleep(1000);
+        }
+    }
+}
