fc5b7ea16cc1f1071608aec72c0346a7f8e283fa
Eduardo Gasparotto <gasp.eduardo@gmail.com>
2020-06-26 23:56:11 -0300

    Removido bug ao atualizar a tela

---
+++ ProjetoIA.Apresentacao/Models/InformacoesDaTela.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ProjetoIA.Apresentacao.Models
+{
+    public class InformacoesDaTela : INotifyPropertyChanged
+    {
+        private int numeroDeGeracoes;
+
+        public Int32 NumeroDeGeracoes
+        {
+            get
+            {
+                return numeroDeGeracoes;
+            }
+            set
+            {
+                numeroDeGeracoes = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public InformacoesDaTela()
+        {
+            NumeroDeGeracoes = 0;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void IncrementarGeracao()
+        {
+            NumeroDeGeracoes++;
+        }
+
+    }
+}
